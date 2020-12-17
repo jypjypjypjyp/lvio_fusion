07cbf7ce51d2ed70794c692879b075c1594c2be3
Jia Yupeng <jypjypjypjyp@hotmail.com>
2020-12-17 16:52:59 +0800

    environment

---
+++ src/lvio_fusion/src/environment.cpp
@@ -0,0 +1,11 @@
+#include "lvio_fusion/adapt/environment.h"
+
+namespace lvio_fusion
+{
+
+void Environment::Interact(Frame::Ptr frame, Weights *weights, double *reward)
+{
+    
+}
+
+} // namespace lvio_fusion
